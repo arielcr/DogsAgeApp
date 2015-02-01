f380e195965cd5be503795edb694160873d57d8e
Ariel Orozco <arielorozco@gmail.com>
2015-02-01 16:14:44 -0600

    Initial Commit

---
+++ DogsAge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DogsAge
+//
+//  Created by Ariel Orozco on 2/1/15.
+//  Copyright (c) 2015 Ariel Orozco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
