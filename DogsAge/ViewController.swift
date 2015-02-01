d6583baabb52e913a37d97e8d36ddacc39576555
Ariel Orozco <arielorozco@gmail.com>
2015-02-01 16:59:03 -0600

    Initial Commit

--- DogsAge/ViewController.swift
+++ DogsAge/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogsAgeTextField: UITextField!
+    @IBOutlet weak var resultsTextField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateButton(sender: UIButton) {
+        
+        var dogsAge = dogsAgeTextField.text.toInt()!
+        
+        var humanAge = dogsAge * 7
+        
+        resultsTextField.hidden = false
+        resultsTextField.text = "Your dog's human age is " + "\(humanAge)" + " years."
+        
+    }
 
 }
 
