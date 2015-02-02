d124a90148659c1de87a9fa68cc423984ee08460
Ariel Orozco <arielorozco@gmail.com>
2015-02-01 18:36:59 -0600

    With complex formula

--- DogsAge/ViewController.swift
+++ DogsAge/ViewController.swift
@@ -25,14 +25,33 @@ class ViewController: UIViewController {
 
     @IBAction func calculateButton(sender: UIButton) {
         
-        var dogsAge = dogsAgeTextField.text.toInt()!
+        var dogsAge = Double((dogsAgeTextField.text as NSString).doubleValue)
         
         var humanAge = dogsAge * 7
         
         resultsTextField.hidden = false
-        resultsTextField.text = "Your dog's human age is " + "\(humanAge)" + " years."
+        resultsTextField.text = "Your dog's simple human age is " + "\(humanAge)" + " years."
         
     }
 
+    @IBAction func calculateComplex(sender: UIButton) {
+        
+        var dogsAge = Double((dogsAgeTextField.text as NSString).doubleValue)
+        var humanAge:Double
+        
+        if dogsAge > 2 {
+            humanAge = (10.5 * 2) + (dogsAge - 2) * 4
+        }
+        else {
+            humanAge = dogsAge * 10.5
+        }
+        
+        resultsTextField.hidden = false
+        resultsTextField.text = "Your dog's complex human age is " + "\(humanAge)" + " years."
+        
+        // Hide keyboard
+        dogsAgeTextField.resignFirstResponder()
+        
+    }
 }
 
